62755816b18705402f4eef738b43d83e4ba20d7a
shohan <mahmudulalam186@gmail.com>
2023-02-27 21:59:04 +0600

    module-05 assignment

---
+++ module-05_assignment/Display.php
@@ -0,0 +1,3 @@
+//Task 3: Part-02
+<?php
+
---
+++ module-05_assignment/data_input.php
@@ -0,0 +1,6 @@
+//Task-3 part 1
+<?php
+if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+}
